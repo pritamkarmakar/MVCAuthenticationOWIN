8c1958f55c65f190d81c497827e0b86d43e4187b
Pritam Karmakar <pkarmaka@tibco.com>
2014-08-27 02:33:15 -0700

    Authentication in MVC 5 app using OWIN - first version

---
+++ MVCApp/Controllers/ApiConnectController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using MVCApp.Helper;
+
+namespace MVCApp.Controllers
+{
+    public class ApiConnectController : Controller
+    {
+        private string webAPiUrl = ConfigurationManager.AppSettings["WebApiUrl"];
+        CommonMethods commonMethods = new CommonMethods();
+
+        /// <summary>
+        /// Action to get the response from '/api/values' API, and this Web API has the [Authorize] attribute
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult GetValues()
+        {
+            // Retrieving the Bearer token that we saved in 'AccountController/Login' method
+            var identity = (ClaimsIdentity)User.Identity;
+            IEnumerable<Claim> claims = identity.Claims;
+
+            var token = claims.First(m => m.Type == "Token").Value;
+
+            string response = commonMethods.HttpGETResponse(webAPiUrl + "/api/values", token);
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
